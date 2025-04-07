38c34950ea7afda621d5603c463bfb17b7adf220
Can Karahan <cankarahan200361@hotmail.com>
2025-04-07 23:48:01 +0300

    Yemek menüsü interaktif sistem eklendi

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,18 +1,209 @@
-const yemekListesi = [
-    {
-      tarih: "01.04.2025",
-      gun: "Pazartesi",
-      icerik: ["Ramazan Bayramı'nın 3. Günü"],
-      kalori: "Kalori"
-    },
-    {
-      tarih: "04.04.2025",
-      gun: "Cuma",
-      icerik: ["Yayla Çorbası", "Tavuk Sote", "Fırın Makarna", "Armut"],
-      kalori: "1060 Kalori"
-    },
-    // Diğer günler
-  ];
-  
-  export default yemekListesi;
-  
+const mealData = [
+  {
+    date: "01.04.2025",
+    items: [
+      "RAMAZAN BAYRAMI'NIN 3. GÜNÜ",
+    "Kalori"
+    ]
+  },
+  {
+    date: "02.04.2025",
+    items: [
+      "İDARİ İZİN",
+    "Kalori"
+    ]
+  },
+  {
+    date: "03.04.2025",
+    items: [
+      "İDARİ İZİN",
+    "Kalori"
+    ]
+  },
+  {
+    date: "04.04.2025",
+    items: [
+      "İDARİ İZİN",
+    "Kalori"
+    ]
+  },
+  {
+    date: "07.04.2025",
+    items: [
+      "Düğün Çorbası",
+    "Etsiz Kuru Fasulye",
+    "Pirinç Pilavı",
+    "Yoğurt",
+    "1017 Kalori"
+    ]
+  },
+  {
+    date: "08.04.2025",
+    items: [
+      "Mercimek Çorbası",
+    "Ekşili Köfte",
+    "Soslu Makarna",
+    "Supangle",
+    "1100 Kalori"
+    ]
+  },
+  {
+    date: "09.04.2025",
+    items: [
+      "Kaşarlı Domates Çorbası",
+    "İslim Kebabı",
+    "A. Şehriyeli Pirinç Pilavı",
+    "Cacık",
+    "1110 Kalori"
+    ]
+  },
+  {
+    date: "10.04.2025",
+    items: [
+      "Yayla Çorbası",
+    "Tavuk Sote (Patates Garnili)",
+    "Fırın Makarna",
+    "Armut",
+    "1060 Kalori"
+    ]
+  },
+  {
+    date: "11.04.2025",
+    items: [
+      "Ezogelin Çorbası",
+    "Patates Oturtma",
+    "Yoğurtlu Semizotu Salatası",
+    "Aşure",
+    "1118 Kalori"
+    ]
+  },
+  {
+    date: "14.04.2025",
+    items: [
+      "Düğün Çorbası",
+    "Etsiz Nohut Yemeği",
+    "Pirinç Pilavı",
+    "Havuç Tarator",
+    "965 Kalori"
+    ]
+  },
+  {
+    date: "15.04.2025",
+    items: [
+      "Sebze Çorbası (Yaz)",
+    "Piliç Topkapı",
+    "Zeytinyağlı Mantar Sote",
+    "Cevizli Baklava",
+    "1182 Kalori"
+    ]
+  },
+  {
+    date: "16.04.2025",
+    items: [
+      "Mercimek Çorbası",
+    "Kıymalı Biber Dolması (Yoğurt Garnili)",
+    "Peynirli Kol Böreği",
+    "Çilek",
+    "1101 Kalori"
+    ]
+  },
+  {
+    date: "17.04.2025",
+    items: [
+      "Şehriyeli Tavuksuyu Çorbası",
+    "Çoban Kavurma",
+    "Pirinç Pilavı",
+    "Yoğurt",
+    "1103 Kalori"
+    ]
+  },
+  {
+    date: "18.04.2025",
+    items: [
+      "Toyga Çorbası",
+    "Soslu Misket Köfte (Püre Garnili)",
+    "Cevizli Erişte",
+    "Mevsim Salata",
+    "1030 Kalori"
+    ]
+  },
+  {
+    date: "21.04.2025",
+    items: [
+      "Ezogelin Çorbası",
+    "Etsiz Bezelye Yemeği",
+    "Kıymalı Talaş Böreği",
+    "Ayran",
+    "1015 Kalori"
+    ]
+  },
+  {
+    date: "22.04.2025",
+    items: [
+      "Kremalı Mantar Çorbası",
+    "Tavuk Külbastı (Patates Garnili)",
+    "Peynirli Makarna",
+    "Fındıklı Şekerpare",
+    "1140 Kalori"
+    ]
+  },
+  {
+    date: "23.04.2025",
+    items: [
+      "23 NİSAN ULUSAL EGEMENLİK ve ÇOCUK BAYRAMI",
+    "Kalori"
+    ]
+  },
+  {
+    date: "24.04.2025",
+    items: [
+      "Mercimek Çorbası",
+    "Etli Taze Fasulye",
+    "Pirinç Pilavı",
+    "Muz",
+    "960 Kalori"
+    ]
+  },
+  {
+    date: "25.04.2025",
+    items: [
+      "Sebze Çorbası (Yaz)",
+    "Filiz Kebabı",
+    "Arpa Şehriye Pilavı",
+    "Akdeniz Salatası",
+    "1054 Kalori"
+    ]
+  },
+  {
+    date: "28.04.2025",
+    items: [
+      "Düğün Çorbası",
+    "Tereyağlı Kuru Fasulye",
+    "Pirinç Pilavı",
+    "Yoğurt",
+    "989 Kalori"
+    ]
+  },
+  {
+    date: "29.04.2025",
+    items: [
+      "Kaşarlı Domates Çorbası",
+    "Talaş Kebabı",
+    "Yoğurtlu Kapa",
+    "Fıstıklı Şöbiyet (Kaymaklı)",
+    "1226 Kalori"
+    ]
+  },
+  {
+    date: "30.04.2025",
+    items: [
+      "Mercimek Çorbası",
+    "Patlıcan Musakka",
+    "Arpa Şehriyeli Pirinç Pilavı",
+    "Cacık",
+    "1069 Kalori"
+    ]
+  },
+];
+
+export default mealData;
